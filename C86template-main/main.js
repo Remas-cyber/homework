940005200309640a07997a616c431756fa679fd5
Remas-cyber <78889235+Remas-cyber@users.noreply.github.com>
2021-11-22 13:12:02 -0800

    Add files via upload

---
+++ C86template-main/main.js
@@ -0,0 +1,14 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+	
+}
+
+function playSound(){
+    
+        x.play();
+  	
+}
